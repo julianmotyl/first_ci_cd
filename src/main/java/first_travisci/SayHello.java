96f3b71aa396b550221813187fb5ba23966ebfa8
julianmotyl <46448121+julianmotyl@users.noreply.github.com>
2021-05-26 10:56:41 +0200

    Update SayHello.java
    Changement pour faire sauter les tests

--- src/main/java/first_travisci/SayHello.java
+++ src/main/java/first_travisci/SayHello.java
@@ -2,7 +2,7 @@ package first_travisci;
 
 public class SayHello {
 
-	public static final String HELLO = "Hello ";
+	public static final String HELLO = "Bien le bonjour ";
 	
 	public static final String EXCLAMATION = " !";
 	
